6d45ab27f28cef169047fdd41a1acda0a4187c69
myousaf3 <132670208+myousaf3@users.noreply.github.com>
2023-06-19 16:06:57 +0500

    Update urls.py

--- exam/urls.py
+++ exam/urls.py
@@ -4,7 +4,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path("", views.home, name="home"),
+    path("", views.user_login, name="login"),
     path("signup/", views.signup, name="signup"),
     path("login/", views.user_login, name="login"),
     path("logout/", views.user_logout, name="logout"),
