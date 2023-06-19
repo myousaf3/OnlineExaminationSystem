b96aa89d4f6fbab949cba25cdeff29ad95efe9f7
myousaf3 <132670208+myousaf3@users.noreply.github.com>
2023-06-19 17:51:33 +0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,4 +2,7 @@
 # Install dependencies
 pip install -r requirements.txt
 # Run database migrations
-python manage.py createsuperuser --no-input
+if [[ $CREATE_SUPERUSER ]];
+then
+  python manage.py createsuperuser --no-input
+fi
